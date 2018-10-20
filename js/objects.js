3aef32ad78d340ac0a6a0e3efa7457a5fbde15d9
negreteo <oscar.negrete09@gmail.com>
2018-10-20 11:56:44 -0400

    Method chaining

--- js/objects.js
+++ js/objects.js
@@ -3,24 +3,26 @@ class User {
   constructor(email, name) {
     this.email = email;
     this.name = name;
+    this.score = 0
   }
 
   login() {
     console.log(this.email, 'just logged in');
+    return this;
   }
   logout() {
     console.log(this.email, 'has logged out');
+    return this;
   }
+  updateScore() {
+    this.score++;
+    console.log(this.email, 'score is now', this.score);
+    return this; // return instance of the object, required for chaining
+  }
+}
 
-};
-
-// the 'new' keyword
-// - creates a new empty object {}
-// - sets the value of 'this' to be the new empty object
-// - calls the constructor method
 
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariocorp.com', 'Yoshi');
 
-userOne.login();
-userTwo.logout();
+userOne.login().updateScore().updateScore().logout();
