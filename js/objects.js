bd5b730928ad9af16441bfb9dafa8a945fa7de61
negreteo <oscar.negrete09@gmail.com>
2018-10-20 13:46:09 -0400

    constructor under hood

--- js/objects.js
+++ js/objects.js
@@ -1,43 +1,15 @@
-class User {
+function User(email, name) {
+  this.email = email;
+  this.name = name;
+  this.online = false;
 
-  constructor(email, name) {
-    this.email = email;
-    this.name = name;
-    this.score = 0
-  }
-
-  login() {
-    console.log(this.email, 'just logged in');
-    return this;
-  }
-  logout() {
-    console.log(this.email, 'has logged out');
-    return this;
-  }
-  updateScore() {
-    this.score++;
-    console.log(this.email, 'score is now', this.score);
-    return this; // return instance of the object, required for chaining
+  this.login = function () {
+    console.log(this.email, "has logged in")
   }
 }
-
-// admin inherits user
-class Admin extends User {
-  deleteUser(user) {
-    users = users.filter(u => {
-      return u.email != user.email;
-    })
-  }
-}
-
 
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariocorp.com', 'Yoshi');
 
-var admin = new Admin('mario@mariocorp.com', 'Mario');
-
-var users = [userOne, userTwo, admin]
-
-//user.deleteUser(userOne); // will not work
-admin.deleteUser(userOne);
-console.log(users);
+console.log(userOne);
+userTwo.login();
