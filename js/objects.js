fd90df4a07cac0e222ab01f5c02f0a814152f3a2
negreteo <oscar.negrete09@gmail.com>
2018-10-20 17:26:46 -0400

    Prototype inheritance

--- js/objects.js
+++ js/objects.js
@@ -16,9 +16,29 @@ User.prototype.logout = function () {
   console.log(this.email, "has logged out")
 }
 
+function Admin(...args) {
+  //console.log(args);
+  User.apply(this, args);
+  this.role = 'super admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function (u) {
+  users = users.filter(user => {
+    return user.email != u.email;
+  })
+};
+
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariocorp.com', 'Yoshi');
+var admin = new Admin('mario@mariocorp.com', 'Mario');
 
-console.log(userTwo);
-userTwo.login();
-userTwo.logout();
+var users = [userOne, userTwo, admin];
+
+console.log(userOne);
+console.log(admin);
+console.log(admin.login());
+console.log(users);
+admin.deleteUser(users[1]);
+console.log(users)
