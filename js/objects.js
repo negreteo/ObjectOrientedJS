84d13e8829890c3dba56ae4f88d6375b2a7524c4
negreteo <oscar.negrete09@gmail.com>
2018-10-20 13:56:31 -0400

    prototype

--- js/objects.js
+++ js/objects.js
@@ -1,15 +1,24 @@
+// constructor function
 function User(email, name) {
   this.email = email;
   this.name = name;
   this.online = false;
+}
 
-  this.login = function () {
-    console.log(this.email, "has logged in")
-  }
+// prototype property 
+User.prototype.login = function () {
+  this.online = true;
+  console.log(this.email, "has logged in")
+}
+
+User.prototype.logout = function () {
+  this.online = false;
+  console.log(this.email, "has logged out")
 }
 
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariocorp.com', 'Yoshi');
 
-console.log(userOne);
+console.log(userTwo);
 userTwo.login();
+userTwo.logout();
